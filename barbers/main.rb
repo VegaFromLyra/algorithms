6c9893a1198d95b9f7936129529ea978fc19d839
ashab <asha.ab@gmail.com>
2017-02-12 11:54:38 -0500

    First take at file processing script

---
+++ barbers/main.rb
@@ -0,0 +1,24 @@
+load 'scheduler.rb'
+
+input_lines = File
+  .readlines("input.txt")
+  .map { |line| line.gsub(/\n/, "") } 
+
+number_of_test_cases = input_lines.first
+
+test_cases = input_lines - [number_of_test_cases]
+
+number_of_test_cases.to_i.times do |test_case_number|
+  test_case_input = test_cases.take(2)
+  test_cases = test_cases - test_case_input
+
+  customer_number = test_case_input[0].split.last
+  barber_times = test_case_input[1].split.map { |entry| entry.to_i }
+
+  output = Scheduler
+    .new(barber_times: barber_times)
+    .find_barber(customer_number: customer_number)
+
+  puts "Case #{test_case_number + 1}: #{output}"
+end
+
