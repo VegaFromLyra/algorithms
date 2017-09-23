6d50cbb42006a8b4d1eb1157a241cdd1e2ad2092
ashab <asha.ab@gmail.com>
2017-09-23 02:02:14 -0400

    TIL how inject works in Ruby

--- top_n_words/top_n_words.rb
+++ top_n_words/top_n_words.rb
@@ -7,23 +7,24 @@ end
 def parse(sentence)
   delimiters = /[.,?!':]/
   words = sentence.gsub(delimiters, "").split
-  words.each { |word| word.downcase! }
+  words.each { |word| word.downcase! }.sort
 end
 
 def build_freq_map(words)
-  # TODO: How does the += work on nil
-  words.inject(Hash.new(0)) { |output, word| output[word] += 1; output }
+  words.inject(Hash.new(0)) do |output, word|
+    output[word] += 1
+    output
+  end
 end
 
 def pick_top_n(freq_map, n_words)
   freq_map
     .sort_by { |key, value| value }
     .reverse
-    .to_h
-    .keys
+    .map { |key, value| key }
     .take(n_words)
 end
 
 str = "Two vast and trunkless legs of stone Stand in the desert. Near them, on the sand, Half sunk, a shattered visage lies, whose frown, And wrinkled lip, and sneer of cold command, Tell that its sculptor well those passions read Which yet survive, stamped on these lifeless things, The hand that mocked them and the heart that fed: And on the pedestal these words appear: 'My name is Ozymandias, king of kings: Look on my works, ye Mighty, and despair!' Nothing beside remains. Round the decay Of that colossal wreck, boundless and bare The lone and level sands stretch far away."
 
-puts top_n_words(str,10)
+puts top_n_words(str, 10)
