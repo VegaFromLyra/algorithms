7b23a3aae1f438b36bb540565c95bc4e0f2a0c6a
ashab <asha.ab@gmail.com>
2018-10-27 16:26:08 -0400

    wip

--- eggs.rb
+++ eggs.rb
@@ -1,25 +1,26 @@
 def highest_floor(number_of_floors)
-  lowest_floor = 0
-  highest_floor = number_of_floors - 1
+  lowest_floor = 1
+  highest_floor = number_of_floors
   number_of_drops = 0
 
-  loop do
+  while (lowest_floor + 1) < highest_floor
+    puts "Lowest floor: #{lowest_floor}"
+    puts "Highest floor: #{highest_floor}"
+
     middle_floor = (lowest_floor + highest_floor) / 2
 
-    if (middle_floor == lowest_floor) || (middle_floor == highest_floor)
-      return (middle_floor + 1)
+    number_of_drops += 1
+
+    puts "Drop from #{middle_floor}: #{number_of_drops}"
+
+    if egg_breaks?(middle_floor)
+      highest_floor = middle_floor
     else
-      number_of_drops += 1
-
-      puts "Drop: #{number_of_drops}"
-
-      if egg_breaks?(middle_floor)
-        highest_floor = middle_floor
-      else
-        lowest_floor = middle_floor
-      end
+      lowest_floor = middle_floor
     end
   end
+
+  middle_floor
 end
 
 def egg_breaks?(floor)
